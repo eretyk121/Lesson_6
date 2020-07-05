0a46a41a8dba0e1f6049a3c94f6a98abfc9a399b
eretyk121 <58330074+eretyk121@users.noreply.github.com>
2020-07-05 14:48:33 +0300

    Update forms.py

--- car_admin/app/forms.py
+++ car_admin/app/forms.py
@@ -4,15 +4,6 @@ from .models import Review, Car
 from ckeditor.widgets import CKEditorWidget
 
 
-# class CarAdminForm(forms.ModelForm):
-#     class Meta:
-#         model = Review
-#         fields = ['car', 'id', ]
-#     def clean(self):
-#         cleaned_data = self.cleaned_data['id']
-#         return cleaned_data
-
-
 class ReviewAdminForm(forms.ModelForm):
     text = forms.CharField(label='Текст', widget=CKEditorWidget())
 
